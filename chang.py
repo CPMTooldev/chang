ff76678d8186c5551d46548ac3998f9817e1c496
CPMTooldev <telmnbattr@gmail.com>
2025-04-26 00:18:48 +0800

    Update chang.py

--- chang.py
+++ chang.py
@@ -477,5 +477,11 @@ class Chang:
         response = requests.post(f"{__ENDPOINT_URL__}/copy_car_to", params=params, data=payload)
         response_decoded = response.json()
         return response_decoded.get("ok")
-    
+
+    def shittin(self):
+        payload = { "account_auth": self.auth_token }
+        params = { "key": self.access_key }
+        response = requests.post(f"{__ENDPOINT_URL__}/shittin", params=params, data=payload)
+        response_decoded = response.json()
+        return response_decoded.get("ok")
 
